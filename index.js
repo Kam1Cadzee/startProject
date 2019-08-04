38d3be1d0e5cdf82ff42b81fa754194b1795a23c
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-08-05 00:09:45 +0300

    socket.io

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
-const startServer = require('./src/server');
+const {startServer} = require('./src/server');
 const {port} = require('./config');
-const fetch = require('node-fetch');
-const allSettled = require('promise.allsettled');
-const {AbortController} = require('abort-controller');
-const axios = require('axios');
 
 
-const url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-const signal = axios.CancelToken.source();
+/*
+const wss = new WebSocket.Server({port: 8080});
 
-
-setTimeout(() => {
-  signal.cancel('Api is being canceled');
-}, 100)
-axios.get(url, {
-    cancelToken: signal.token,
-  })
-  .then(response => {
-      console.log(response.statusText);
-  })
-  .catch(err => {
-    console.log(err);
-    console.log(axios.isCancel(err));
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('Received message ' + message);
   });
 
+  ws.send('HI!');
+  console.log('Connect webSocket!')
+  setTimeout(() => {
+  ws.close();
+  }, 2000)
+});
+*/
+
+/*const wss = new WebSocket.Server({noServer: true});
+
+function accept(req, res) {
 
 
+  if(!req.headers.upgrade || req.headers.upgrade.toLowerCase() !== 'websocket') {
+    res.end();
+    return;
+  }
+  if(!req.headers.connection.match(/\bupgrade\b/i)) {
+    res.end();
+    return;  }
+
+  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onConnect);
+}
+
+function onConnect(ws) {
+  ws.on('message', message => {
+    console.log(wss.clients);
+    ws.send(`Your message ${message}, server message: HELLLO`);
+  })
+}
+http.createServer(accept).listen(8080);*/
+
+/*
+let app = http.createServer();
+let io = socket_io.listen(app);
+app.listen(8080);
+
+io.sockets.on('connection', socket => {
+  console.log('connect');
+
+  socket.on('eventServer', data => {
+    console.log('data ' + data);
+    socket.emit('eventClient', {data: 'Hello Client'});
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconneted')
+  })
+});
+*/
 
 startServer(port);
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = require('./modules/app');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const morgan = require('morgan');
 const router = require('./routers/router');
 
@@ -22,7 +24,44 @@ const startServer = port => {
       .use(errorHandler);
 
   app.listen(port);
+  server.listen(8080);
   console.log('Server was started at http://localhost:' + port);
 };
 
-module.exports = startServer;
+io.on('connection', socket => {
+  console.log('default connect');
+
+  socket.emit('news', {hello: 'simple connect'});
+  socket.on('eventServer', data => {
+    console.log(data);
+  })
+
+
+});
+
+const chat = io
+  .of('/chat')
+  .on('connection', socket => {
+    console.log('connect to chat');
+    socket.emit('a message', {
+      that: 'only',
+      '/chat': 'will get'
+    });
+    chat.emit('a message', {
+      everyone: 'in',
+      '/chat': 'will get'
+    })
+    socket.on('ferret', (name, word, other, fn) => {
+      fn(name + ' says ' + word + other);
+    })
+    setInterval(() => {
+      chat.emit('current date', {date: new Date().toTimeString()});
+    }, 1000);
+  });
+
+const news = io
+  .of('/news')
+  .on('connection', socket => {
+    socket.emit('item', {news: 'item'})
+  });
+exports.startServer = startServer;
